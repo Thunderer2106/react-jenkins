ce1e98af5d6055786181afc5d1366114ed5059c4
Thunderer2106 <akashkaramsetty@gmail.com>
2024-11-08 13:48:40 +0530

    second react commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React!
+          Learning Reactt!
         </a>
       </header>
     </div>
